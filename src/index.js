3afc3094f8eeee119d2de469b28c49033a139419
Turpal <voin_957@mail.ru>
2024-07-29 22:32:55 +0300

    feat: finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  for (let i = 0; i < String(n).length; i++) {
+    if(String(n)[i] !== '-') result = String(n)[i] + result;
+  }
+  console.log(result)
+  return +result;
 }
